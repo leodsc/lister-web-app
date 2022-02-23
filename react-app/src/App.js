3be001bb6b41b17055bb35dfff954067470e741f
Leonardo <leodscarvalho@gmail.com>
2022-02-22 21:38:09 -0300

    Add: created react app and started front end

---
+++ react-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './App.css';
+
+class App extends React.Component() {
+  render() {
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  }
+}
+
+export default App;
