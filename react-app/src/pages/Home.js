a269a265130fbe6fcac2e2669496e2748f37b0be
Leonardo <leodscarvalho@gmail.com>
2022-03-02 23:08:45 -0300

    add: responsive card container and svgs for design

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
+import { ReactComponent as CloseSVG } from '../assets/Close.svg';
+import { ReactComponent as CheckmarkSVG } from '../assets/checkmark.svg';
 
 import cards from '../test/cards.js';
 
@@ -6,16 +8,33 @@ import "../styles/home.css";
 
 function Home() {
   const [currentCardOpen, setCurrentCardOpen] = useState(null);
+  const cardsCtn = useRef(null);
+
+  const changeCardsCtnDisplay = (remove, add) => {
+    cardsCtn.current.classList.remove("cards-ctn__" + remove);
+    cardsCtn.current.classList.add("cards-ctn__" + add);
+  }
+
+  useEffect(() => {
+    const isCardClosed = currentCardOpen === null;
+
+    if (isCardClosed) {
+      changeCardsCtnDisplay("flex", "grid");
+    } else {
+      changeCardsCtnDisplay("grid", "flex");
+    }
+  }, [currentCardOpen])
 
   return (
     <main className="home-page flex">
       <ShowAllTasksButton />
-      <div class="cards-ctn">
+      <button className='btn-confirm'>+ Card</button>
+      <div ref={cardsCtn} class="cards-ctn">
         {cards.map(card => {
           return <Card content={card} setCurrentCardOpen={setCurrentCardOpen} />
         })}
       </div>
-      {currentCardOpen !== null && <CardOpen card={currentCardOpen} />}
+      {currentCardOpen !== null && <CardOpen setCurrentCardOpen={setCurrentCardOpen} card={currentCardOpen} />}
     </main>
   )
 }
@@ -46,24 +65,65 @@ function Card(props) {
     <div onClick={() => {
       setCurrentCardOpen(content);
     }} className="home-page--card">
-      <h1 className="card-title">{content.name}</h1>
+      <TasksProgression percentage={content.done / content.total} />
+      <p className="card-title">{content.name}</p>
     </div>
   )
 
 }
 
 function CardOpen(props) {
-  const { card } = props;
+  const { card, setCurrentCardOpen } = props;
+
+  const updateCard = (task) => {
+    task.done = !task.done;
+    task.done ? card["done"]++ : card["done"]--;
+    console.log(card);
+  }
 
   return (
-    <div>
-      <div>
-        <h1>{card.name}</h1>
+    <div className='card-open'>
+      <div className="card-open--info">
+        <p class="card-percentage">{((card.done / card.total) * 100).toFixed(2)}%</p>
+        <h2 className='card-open--title'>{card.name}</h2>
+        <CloseSVG className='card-open--close' onClick={() => {
+          setCurrentCardOpen(null);
+        }} />
       </div>
-      {card.tasks.map(task => {
-        return <div><h3>{task.name}</h3></div>
-      })}
+      <button className='btn-confirm task-btn'>+ Tarefa</button>
+      <div className='card-open--tasks'>
+        {card.tasks.map(task => {
+          return (
+            <div
+              onDoubleClick={() => updateCard(task)}
+              className='card-open--task-ctn'>
+              <CheckmarkSVG />
+              <p className='task'>{task.name}</p>
+            </div>)
+        })}
+      </div>
     </div>
+  )
+}
+
+function TasksProgression(props) {
+  const graph = useRef(null);
+  const data = props.percentage;
+
+  useEffect(() => {
+    console.log(data);
+    if (graph.current.getContext('2d')) {
+      const ctx = graph.current.getContext('2d');
+      const circle = new Path2D();
+      circle.arc(20, 20, 15, 0, Math.PI * 2 * data);
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "#44bba4";
+      ctx.stroke(circle);
+    }
+  })
+
+  return (
+    <canvas ref={graph} class="card-progression" width="40" height="40"></canvas>
   )
 }
 
--- react-app/src/test/cards.js
+++ react-app/src/test/cards.js
@@ -1,20 +1,28 @@
 const cards = [
   {
     "name": "trabalho",
-    "total": 10,
-    "done": 6,
+    "total": 3,
+    "done": 1,
     "tasks": [
       {
         "name": "teste nao feito",
+        "id": 1,
         "done": false
       },
       {
-        "name": "outro teste nao feito",
+        "name": "outro teste nao lorem ipsum weofnwe fewnfe #44bba4 #44bba4 #44bba4 #44bba4#44bba4 ########ppp44bba444bba444bba444bba444bba444bba444bba444bba4#44bba4#44bba4#44bba4   #44bba4 #44bba4 #44bba4kfewoj  feito",
+        "id": 2,
         "done": false
       },
       {
         "name": "teste feito",
+        "id": 3,
         "done": true
+      },
+      {
+        "name": "mmm",
+        "id": 4,
+        "done": false
       }
     ]
   },
@@ -25,10 +33,29 @@ const cards = [
     "tasks": [
       {
         "name": "teste nao feitoooo",
+        "id": 1,
         "done": false
       },
       {
         "name": "outro faculdade nao feito",
+        "task": 2,
+        "done": false
+      },
+    ]
+  },
+  {
+    "name": "teste",
+    "total": 2,
+    "done": 0,
+    "tasks": [
+      {
+        "name": "teste nao feitoooo",
+        "id": 1,
+        "done": false
+      },
+      {
+        "name": "outro faculdade nao feito",
+        "task": 2,
         "done": false
       },
     ]
